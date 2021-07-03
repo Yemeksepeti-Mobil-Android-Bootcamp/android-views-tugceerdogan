6b964807d03c8cb17b48ccb5b872599ef5383960
tugceerdogan <tugce.0483@gmail.com>
2021-07-04 01:25:43 +0300

    Sign In Screen designed and switched between screens

--- app/src/main/java/com/example/advancedviews/MainActivity.kt
+++ app/src/main/java/com/example/advancedviews/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.advancedviews
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtLogin : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtLogin = findViewById<TextView>(R.id.text_login)
+
+
+            txtLogin.setOnClickListener {
+                startActivity(Intent(this@MainActivity, SignInActivity::class.java))
+
+            }
     }
 }
